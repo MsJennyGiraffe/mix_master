691c154a889c3ee5b2e4b10580b064fc60ad9ede
Jennifer <jennifersoden@hotmail.com>
2016-05-29 13:44:02 -0600

    Add homepage
    Added a homepage and links to playlists, songs, and artists
    Added homepage link in playlists, songs, and artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  root to: "home#index"
+
   resources :artists do
     resources :songs, only: [:new, :create]
   end
 
-  resources :songs, only: [:show]
+  resources :songs, only: [:show, :index]
   resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
 end
